d07cdd259c5d94ca280713a9b8380c7192da4de1
scorbett123 <50634068+scorbett123@users.noreply.github.com>
2023-05-22 12:47:38 +0100

    Improve flac_to_wav dataset processing
    Don't spam ffmpeg output, but still print file names

--- datasets/flac_to_wav.sh
+++ datasets/flac_to_wav.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-folder=/home/sam/ml/nea/datasets/dev-other
+folder=LibriSpeech/dev-clean
 
 for file in $(find "$folder" -type f -iname "*.flac")
 do
     name=$(basename "$file" .flac)
     dir=$(dirname "$file")
-    ffmpeg -i "$file" "speech/$name".wav
+    ffmpeg -i "$file" "speech/$name".wav -hide_banner -loglevel error
+    echo "$name"
     #ffmpeg -i $file $dir/$name.wav
     #rm $file
 done
