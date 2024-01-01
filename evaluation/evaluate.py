91686c7d18ecbd46c722954d804f8d3ab4293de5
Sam Corbett <sam@scorbett123.dev>
2024-01-01 18:55:22 +0000

    evaluation update

--- evaluation/evaluate.py
+++ evaluation/evaluate.py
@@ -1,7 +1,5 @@
-from scipy.io import wavfile
 from pesq import pesq
 import glob
-import os
 from tqdm import tqdm
 import random
 from encodec.utils import convert_audio
@@ -11,23 +9,21 @@ import torchaudio
 import math
 import torch
 
-model = Models.load("model_saves/low_qual.saved", device="cuda")
-model._load_from_state_dict
+from model.utils import norm
+
+model = Models.load("model_saves/highqual.saved", device="cuda")
 
 audio_files = random.sample(glob.glob("datasets/speech_train/*.wav"), 500)
 values = []
 
 with torch.no_grad():
-    for file in tqdm(audio_files):
+    for file in tqdm(audio_files):  # inefficient to not batch, but this is only iterating over 500, it's fine
         ref, rate = torchaudio.load(file)
         ref = ref.to("cuda")
-
+        ref = norm(ref)
         res = model.forward(ref)[0]
 
         torchaudio.save("evaluation/tmp/test.wav", res.cpu(), rate)
         values.append(pesq(rate, ref.squeeze(0).cpu().numpy(), res.squeeze(0).cpu().numpy(), 'nb'))
-
-        # os.remove(f"evaluation/tmp/{file.split('.')[0].split('/')[-1]}.lyra")
-        # os.remove(f"evaluation/tmp/{file.split('.')[0].split('/')[-1]}_decoded.wav")
 
     print(sum(values) / len(values))
